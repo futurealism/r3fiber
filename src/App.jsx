aaa916f5d0fcecb2b51b243bc4bcec94d3f5ab50
futurealism <futurealismprism@gmail.com>
2023-05-19 14:00:06 -0400

    orbit control customizations

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,12 @@ export default function App() {
       <Polyhedron position={[-0.75, 0.75, 0]} polyhedron={polyhedron} />
       <Polyhedron position={[0.75, 0.75, 0]} polyhedron={polyhedron} />
       <Stats />
-      <OrbitControls />
+      <OrbitControls
+        minAzimuthAngle={-Math.PI / 4}
+        maxAzimuthAngle={Math.PI / 4}
+        minPolarAngle={Math.PI / 6}
+        maxPolarAngle={Math.PI - Math.PI / 6}
+      />
     </Canvas>
   )
 }
