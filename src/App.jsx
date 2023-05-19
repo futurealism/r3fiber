042ed9e57698e2089c8b825abebfba8cb722c38b
futurealism <futurealismprism@gmail.com>
2023-05-19 15:30:40 -0400

    load texture

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,81 @@
-import { Canvas } from '@react-three/fiber'
 import Polyhedron from './Polyhedron'
 import * as THREE from 'three'
-import { useMemo } from 'react'
 import { Stats, OrbitControls } from '@react-three/drei'
+import { Canvas, useLoader } from '@react-three/fiber'
 import { useControls } from 'leva'
+import { useRef } from 'react'
+import Floor from './Floor'
+
+function Lights() {
+  const directionalRef = useRef()
+
+  useControls('Directional Light', {
+    intensity: {
+      value: 1,
+      min: 0,
+      max: 5,
+      step: 0.1,
+      onChange: (v) => {
+        directionalRef.current.intensity = v
+      }
+    },
+
+    position: {
+      x: 3.3,
+      y: 1.0,
+      z: 4.4,
+      onChange: (v) => {
+        directionalRef.current.position.copy(v)
+      }
+    }
+  })
+
+  return <directionalLight ref={directionalRef} castShadow />
+}
 
 export default function App() {
-  const polyhedron = useMemo(
-    () => [
-      new THREE.BoxGeometry(),
-      new THREE.SphereGeometry(0.785398),
-      new THREE.DodecahedronGeometry(0.785398)
-    ],
-    []
-  )
-
-  const options = useMemo(() => {
-    return {
-      x: { value: 0, min: 0, max: Math.PI * 2, step: 0.01 },
-      y: { value: 0, min: 0, max: Math.PI * 2, step: 0.01 },
-      z: { value: 0, min: 0, max: Math.PI * 2, step: 0.01 },
-      visible: true,
-      color: { value: 'lime' }
-    }
-  }, [])
-
-  const pA = useControls('Polyhedron A', options)
-  const pB = useControls('Polyhedron B', options)
+  const texture = useLoader(THREE.TextureLoader, './img/grid.png')
 
   return (
-    <Canvas camera={{ position: [1, 2, 3] }}>
+    <Canvas camera={{ position: [4, 4, 1.5] }} shadows>
+      <Lights />
       <Polyhedron
+        name="meshBasicMaterial"
+        position={[-3, 1, 0]}
+        material={new THREE.MeshBasicMaterial({ map: texture })}
+      />
+      <Polyhedron
+        name="meshNormalMaterial"
         position={[-1, 1, 0]}
-        rotation={[pA.x, pA.y, pA.z]}
-        visible={pA.visible}
-        color={pA.color}
-        polyhedron={polyhedron}
+        material={
+          new THREE.MeshNormalMaterial({
+            flatShading: true
+          })
+        }
       />
       <Polyhedron
+        name="meshPhongMaterial"
         position={[1, 1, 0]}
-        rotation={[pB.x, pB.y, pB.z]}
-        visible={pB.visible}
-        color={pB.color}
-        polyhedron={polyhedron}
+        material={
+          new THREE.MeshPhongMaterial({
+            flatShading: true,
+            map: texture
+          })
+        }
       />
-      <OrbitControls target-y={1} />
+      <Polyhedron
+        name="meshStandardMaterial"
+        position={[3, 1, 0]}
+        material={
+          new THREE.MeshStandardMaterial({
+            flatShading: true,
+            map: texture
+          })
+        }
+      />
+      <Floor />
+      <OrbitControls target={[0, 1, 0]} />
       <axesHelper args={[5]} />
-      <gridHelper />
       <Stats />
     </Canvas>
   )
